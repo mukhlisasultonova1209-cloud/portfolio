48bdc31c01814e274e9215362fe4b9a7bbb7e6c5
Mukhlisa <mukhlisa@gmail.com>
2026-02-03 10:24:28 +0500

    portfolio added

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 AOS.init();
+
+let btn1 = document.getElementById("btn1");
+let btn2 = document.getElementById("btn2");
+let btn3 = document.getElementById("btn3");
+let btn4 = document.getElementById("btn4");
+let btn5 = document.getElementById("btn5");
+
+btn2.addEventListener("click", ()=>{
+    btn1.style.color = `#C6C6C6`
+    btn1.style.backgroundColor = `#FFFFFF14`
+});
